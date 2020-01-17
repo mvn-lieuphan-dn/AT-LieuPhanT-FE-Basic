10c4ee5b84b79a6ff48c1839207ecf91ccef8222
at-lieuphan <lieu.phan@asiantech.vn>
2020-01-17 09:16:45 +0700

    edit function delete product in cart

--- js-ex2 3/cart.js
+++ js-ex2 3/cart.js
@@ -2,7 +2,7 @@ var cartList = JSON.parse(localStorage.getItem('cart'));
 console.log(cartList);
 var $ItemCart = document.getElementById('js-card');
 var subTotal;
-var count, total = 0;
+var count;
 var idcart;
 function listCart() {
   for (var i = 0, leng = cartList.length; i < leng; i++) {
@@ -10,7 +10,6 @@ function listCart() {
     count = parseInt(cartList[i].quantity);
     priceCart = parseInt(cartList[i].product.price);
     subTotal = count * priceCart;
-    total += subTotal;
     $ItemCart.innerHTML += '<tr id ="jsRow' + idcart + '">' +
       '<td>' + idcart + '</td>' +
       '<td>' + cartList[i].product.titleProduct + '</td>' +
@@ -22,28 +21,29 @@ function listCart() {
   }
   document.getElementById('js-total').innerHTML = sumTotal();
 }
-  
-function sumTotal(){
+
+function sumTotal() {
   var sum = 0;
-  for(var j = 0, length = cartList.length; j < length; j++){
+  for (var j = 0, length = cartList.length; j < length; j++) {
     sum += parseInt(cartList[j].quantity) * parseInt(cartList[j].product.price);
-  } 
+  }
   return sum;
 }
 
 listCart();
-function deleteCart(id) {
-  for (var k = 0, length = cartList.length; k < length; k++) {
-    console.log(cartList[k]);
-    if (cartList[k].product.id === id) {
-      var ind = cartList.indexOf(cartList[k]);
-      console.log('index cart: ' + ind);
-      cartList.splice(ind, 1);
-      console.log(cartList);
-      $ItemCart.innerHTML = [];
-      localStorage.setItem('cart', JSON.stringify(cartList));
-      alert('deleted');
-      listCart();
+function deleteCart(idPro) {
+  var message = confirm("Do you want delete this product?");
+  if (message === true) {
+    for (var k = 0, length = cartList.length; k < length; k++) {
+      console.log(cartList[k].product.id);
+      if (cartList[k].product.id === idPro) {
+        cartList.splice(k, 1);
+        $ItemCart.innerHTML = [];
+        localStorage.setItem('cart', JSON.stringify(cartList));
+        listCart();
+        return true;
+      }
     }
   }
+  else return false;
 }
