770699e45622a18c09bf552c08c247fad1103eec
at-lieuphan <lieu.phan@asiantech.vn>
2020-02-04 15:11:20 -0800

    change single quote

--- ts-ex1/common.ts
+++ ts-ex1/common.ts
@@ -12,14 +12,14 @@ class Product {
     this.price = price;
   }
 }
-var product1 = new Product (1, '1.jpg', "Traditional women's long dress, neck jars", 'Color red, handmade embroidery', 200);
-var product2 = new Product (2, '3.jpg', "Traditional women's long dress, neck jars", 'Color white, handmade embroidery', 270);
-var product3 = new Product (3, '4.jpg', "Traditional women's long dress, long hand", 'Color blue, handmade embroidery', 340);
-var product4 = new Product (4, '8.png', "Traditional women's long dress, long hand", 'Color blue, handmade embroidery', 340);
-var product5 = new Product (5, '2.jpg', "Traditional women's long dress, long hand", 'Color white, handmade embroidery', 280);
-var product6 = new Product (6, '5.jpg', "Innovative men's long dress 2", 'Color white, handmade embroidery', 370);
-var product7 = new Product (7, '6.jpg', "Innovative men's long dress 3", 'Color white, handmade embroidery', 330);
-var product8 = new Product (8, '7.jpg', "Innovative men's long dress 4", 'Color white, handmade embroidery', 400);
+var product1 = new Product (1, '1.jpg', 'Traditional women\'s long dress, neck jars', 'Color red, handmade embroidery', 200);
+var product2 = new Product (2, '3.jpg', 'Traditional women\s long dress, neck jars', 'Color white, handmade embroidery', 270);
+var product3 = new Product (3, '4.jpg', 'Traditional women\'s long dress, long hand', 'Color blue, handmade embroidery', 340);
+var product4 = new Product (4, '8.png', 'Traditional women\'s long dress, long hand', 'Color blue, handmade embroidery', 340);
+var product5 = new Product (5, '2.jpg', 'Traditional women\'s long dress, long hand', 'Color white, handmade embroidery', 280);
+var product6 = new Product (6, '5.jpg', 'Innovative men\'s long dress 2', 'Color white, handmade embroidery', 370);
+var product7 = new Product (7, '6.jpg', 'Innovative men\'s long dress 3', 'Color white, handmade embroidery', 330);
+var product8 = new Product (8, '7.jpg', 'Innovative men\'s long dress 4', 'Color white, handmade embroidery', 400);
 console.log(product1);
 var products :Array<string> = [product1, product2, product3, product4, product5, product6, product7, product8];
 console.log(products);
