e24285cb0553f98cd4200f0e97392f201f532416
at-lieuphan <lieu.phan@asiantech.vn>
2020-02-13 14:44:05 -0800

    change class convert interface in common.ts

--- ts-ex1/common.js
+++ ts-ex1/common.js
@@ -1,27 +1,64 @@
-var Product = /** @class */ (function () {
-  function Product(id, imgProduct, titleProduct, descriptionProduct, price) {
-    this.id = id;
-    this.imgProduct = imgProduct;
-    this.titleProduct = titleProduct;
-    this.descriptionProduct = descriptionProduct;
-    this.price = price;
-}
-return Product;
-}());
-var product1 = new Product(1, '1.jpg', 'Traditional women\'s long dress, neck jars', 'Color red, handmade embroidery', 200);
-var product2 = new Product(2, '3.jpg', 'Traditional women\'s long dress, neck jars', 'Color white, handmade embroidery', 270);
-var product3 = new Product(3, '4.jpg', 'Traditional women\'s long dress, long hand', 'Color blue, handmade embroidery', 340);
-var product4 = new Product(4, '8.png', 'Traditional women\'s long dress, long hand', 'Color blue, handmade embroidery', 340);
-var product5 = new Product(5, '2.jpg', 'Traditional women\'s long dress, long hand', 'Color white, handmade embroidery', 280);
-var product6 = new Product(6, '5.jpg', 'Innovative men\'s long dress 2', 'Color white, handmade embroidery', 370);
-var product7 = new Product(7, '6.jpg', 'Innovative men\'s long dress 3', 'Color white, handmade embroidery', 330);
-var product8 = new Product(8, '7.jpg', 'Innovative men\'s long dress 4', 'Color white, handmade embroidery', 400);
-console.log(product1);
-var products = [product1, product2, product3, product4, product5, product6, product7, product8];
+var products = [{
+        id: 1,
+        imgProduct: '1.jpg',
+        titleProduct: 'Traditional women\'s long dress, neck jars',
+        descriptionProduct: 'Color red, handmade embroidery',
+        price: 200
+    },
+    {
+        id: 2,
+        imgProduct: '3.jpg',
+        titleProduct: 'Traditional women\s long dress, neck jars',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 270
+    },
+    {
+        id: 3,
+        imgProduct: '4.jpg',
+        titleProduct: 'Traditional women\'s long dress, long hand',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 340
+    },
+    {
+        id: 4,
+        imgProduct: '8.png',
+        titleProduct: 'Traditional women\'s long dress, long hand',
+        descriptionProduct: 'Color blue, handmade embroidery',
+        price: 340
+    },
+    {
+        id: 5,
+        imgProduct: '2.jpg',
+        titleProduct: 'Traditional women\'s long dress, long hand',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 340
+    },
+    {
+        id: 6,
+        imgProduct: '5.jpg',
+        titleProduct: 'Innovative men\'s long dress 2',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 370
+    },
+    {
+        id: 7,
+        imgProduct: '6.jpg',
+        titleProduct: 'Innovative men\'s long dress 3',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 350
+    },
+    {
+        id: 8,
+        imgProduct: '7.jpg',
+        titleProduct: 'Innovative men\'s long dress 4',
+        descriptionProduct: 'Color white, handmade embroidery',
+        price: 340
+    }];
+//var products :Array<string> = [product1, product2, product3, product4, product5, product6, product7, product8];
 console.log(products);
-function countProCart(arrCart, a) {
-arrCart = JSON.parse(localStorage.getItem('cart'));
-    a = arrCart.length;
+function countProCart() {
+    var arrCart = JSON.parse(localStorage.getItem('cart'));
+    var a = arrCart.length;
     console.log(a);
     document.getElementById('js-icon-amount').innerHTML = a;
 }
